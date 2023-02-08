d048c0c540ec8fcc4fec217cab8b00f71ceeb65a
H4lfbeard <humbert.thomas.pro@gmail.com>
2023-02-08 18:54:29 +0100

    update database

--- lib/database.php
+++ lib/database.php
@@ -2,16 +2,21 @@
 class DatabaseConnection
 {
 	public ?PDO $database = null;
+	
+	
 	/**
 	 * Cette fonction permet de se connecter à la BDD
 	 *
 	 * @return void
 	 */
+
+
 	public function getConnection(): PDO
+
 	{
-    	if ($this->database === null) {
-        	$this->database = new PDO('mysql:host=localhost;dbname=projet5_blog;charset=utf8', 'root', 'root');
-    	}
-    	return $this->database;
+		if ($this->database === null) {
+			$this->database = new PDO( 'mysql:host=localhost;dbname=projet5_blog;charset=utf8', 'root', 'root' );
+		}
+		return $this->database;
 	}
 }
